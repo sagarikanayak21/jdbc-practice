35e365c108c78c6ecdae19c1e1100f15909b952f
sagarika-nayak <sagarikanayak440@gmail.com>
2025-01-10 10:42:18 +0530

    commit from 1st-branch

--- JDBC-1st-project/CurdOperation.java
+++ JDBC-1st-project/CurdOperation.java
@@ -1,5 +1,7 @@
 public class CurdOperation {
     public static void main (String[] args){
         System.out.println("practice commit");
+        
+        System.out.println("HYyyyyyyyyyyyyyyyyyy");
     }
 }
